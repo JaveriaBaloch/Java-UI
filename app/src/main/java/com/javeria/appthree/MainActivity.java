47c85defd5c7af0be292ba72b26fa7a46cbcf07d
Javeria Baloch <65242641+JaveriaBaloch@users.noreply.github.com>
2022-03-23 16:17:51 +0500

    IU file is created, animation and transformation are added

--- app/src/main/java/com/javeria/appthree/MainActivity.java
+++ app/src/main/java/com/javeria/appthree/MainActivity.java
@@ -3,12 +3,23 @@ package com.javeria.appthree;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+TextView textview;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textview = findViewById(R.id.txt2);
+    }
+
+    public void animate(View view) {
+        textview.animate().rotationXBy(120);
+//        textview.animate().rotationYBy(30);
+//        textview.animate().alpha(0).setDuration(4000).start();
+        textview.animate().scaleY(2.2f).scaleX(2.2f).setDuration(4000).start();
+
     }
 }
