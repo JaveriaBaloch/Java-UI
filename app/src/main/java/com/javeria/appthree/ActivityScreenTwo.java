95ccf0345bb5d74b96aab75cf0a9e3eab519b3cf
Javeria Baloch <65242641+JaveriaBaloch@users.noreply.github.com>
2022-03-23 21:15:24 +0500

    navigation is added and text is exported from one screen two other

--- app/src/main/java/com/javeria/appthree/ActivityScreenTwo.java
+++ app/src/main/java/com/javeria/appthree/ActivityScreenTwo.java
@@ -2,13 +2,25 @@ package com.javeria.appthree;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class ActivityScreenTwo extends AppCompatActivity {
-
+    TextView Dis;
+    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_screen_two);
+        String textShow = getIntent().getStringExtra("Some text");
+        Dis= findViewById(R.id.Dis);
+////
+        Dis.setText(textShow);
+
     }
+
+
+
 }
--- app/src/main/java/com/javeria/appthree/MainActivity.java
+++ app/src/main/java/com/javeria/appthree/MainActivity.java
@@ -5,15 +5,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 TextView textview;
+EditText txt1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         textview = findViewById(R.id.txt2);
+        txt1 = findViewById(R.id.txt1);
     }
 
     public void animate(View view) {
@@ -29,6 +35,7 @@ TextView textview;
     public void goToTheSecondScreen(View view) {
 //        Intent intent = new Intent(Start screen.this, navigateScreen.class);
         Intent intent = new Intent(MainActivity.this, ActivityScreenTwo.class);
+        intent.putExtra("Some text",txt1.getText().toString());
 //        startActivity(Intent variable);
           startActivity(intent);
     }
